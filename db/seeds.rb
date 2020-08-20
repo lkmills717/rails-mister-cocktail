9a8c14468ab401a162dc8ec9fa22d14e8ef75cc3
Lara Mills <lkm347@gmail.com>
2020-08-20 16:37:15 +0100

    mister cocktail backend

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+Ingredient.create(name: "Gin")
+Ingredient.create(name: "Sweet vermouth")
+Ingredient.create(name: "Bourbon")
+Ingredient.create(name: "Apricot brandy")
+Ingredient.create(name: "Bitters")
+Ingredient.create(name: "Orange bitters")
